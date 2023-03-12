27c4879d21047cab63c367084349fff8f1bd2eec
leonidandreev <leon.andreew@yandex.ru>
2023-03-13 03:04:57 +0400

    up

--- main.py
+++ main.py
@@ -1,18 +1,38 @@
+import time
 from settings import API_KEY
 import telebot
-# This is a sample Python script.
+import sqlite3
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 bot = telebot.TeleBot(API_KEY)
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+    try:
+        user = message.from_user.id
+        conn = sqlite3.connect('main.db')
+        cursor = conn.cursor()
+        cursor.execute("SELECT id FROM users WHERE telegram_id LIKE " + str(user))
+        results = cursor.fetchall()
+        if (len(results) == 0):
+            print("User not found")
+            cursor.execute("SELECT MAX(id) FROM users")
+            result = cursor.fetchall()
+            for row in result:
+                max = int(row[0]) + 1
+            cursor.execute(
+                "insert into users values (" + str(max) + ", '" + str(user)+"', '') ")
+            conn.commit()
+            bot.reply_to(message,'Приветствуем Вас в нашей команде GENESIS, '+str(message.from_user.first_name)+'! \nНам очень приятно что нас становится все больше!\nПозвольте предложить Вам пройти обучение')
+        if (len(results) > 0):
+            print("User already in system")
+            bot.reply_to(message,str(message.from_user.first_name)+', мы скучали! \nВозвращаем Вас в главное меню!')  # добавить выбор клавы по тарифу функцией getkeys() или подобной
+    except:
+        pass
 
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    while True:
+        try:
+            bot.polling(none_stop=True)
+        except Exception as e:
+            time.sleep(3)
+            print(e)
