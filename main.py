62841d5759c492d2343424d82fc23409922c9993
leonidandreev <leon.andreew@yandex.ru>
2023-03-13 02:41:05 +0400

    up

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
+from settings import API_KEY
+import telebot
 # This is a sample Python script.
 
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
+bot = telebot.TeleBot(API_KEY)
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
